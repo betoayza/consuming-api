dfba106119879b920c375a076c62b48c91e04820
betoayza <aaayza@gmail.com>
2023-03-15 18:51:50 -0300

    File handling added

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
     print(type(data), data)
+    f = open("breeds.txt", "wt")
+    f.write(str(data))
+    f.close()
+    
 else:
     print('Some error has ocurred')
