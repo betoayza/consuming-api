627070cadebbd572d32a122cc582ad3957c57aed
betoayza <aaayza@gmail.com>
2023-03-15 19:08:48 -0300

    File handling updated

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ url = 'https://dog.ceo/api/breeds/list/all'
 response = requests.get(url)
 
 if response.status_code == 200:
-    data = response.json()
+    data = response.json()['message'] # parse JSON to Python Map
+    
     print(type(data), data)
+
+    data2 = []
+
+    for x in data.items():
+        print(x[0])        
+        print(x[1])
+        data2.append(f"{x[0]} : {x[1]}")
+      
     f = open("breeds.txt", "wt")
-    f.write(str(data))
+    f.write(str(data2))
     f.close()
     
 else:
